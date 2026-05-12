342007cd4c0262fb92c853e11de3f0ecf9b9069a
Jenyyk <jendotron2@gmail.com>
2026-05-12 15:10:54 +0200

    dev: add incrementing to range

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A collection of programs i made to make my life easier
 ## programs:
 **charfreq** - counts frequency of characters in files or stdin input
 **pi** - prints pi up to a certain number of digits
+**range** - prints a range of values to stdout (faster than `seq` from GNU/coreutils!)
 
 ## installation:
 ```sh
--- range/src/main.rs
+++ range/src/main.rs
@@ -1,30 +1,59 @@
-unsafe extern "C" {
-    pub unsafe fn printf(fmt: *const u8, ...) -> i32;
-}
-
 use std::{io::Write};
-const FMT: &str = "%d\n";
-const FMT_PTR: *const u8 = FMT.as_ptr();
 
 fn main() {
     let mut args = std::env::args();
     let _program = args.next();
-    let (start, end): (usize, usize) = match (args.next(), args.next()) {
-        (Some(start), Some(end)) => (start.parse().unwrap(), end.parse().unwrap()),
-        (Some(end), None) => (0, end.parse().unwrap()),
-        (None, _) => {
+    let (start, end, step): (usize, usize, usize) = match (args.next(), args.next(), args.next()) {
+        (Some(start), Some(end), Some(step)) => (start.parse().unwrap(), end.parse().unwrap(), step.parse().unwrap()),
+        (Some(start), Some(end), None) => (start.parse().unwrap(), end.parse().unwrap(), 1),
+        (Some(end), None, _) => (0, end.parse().unwrap(), 1),
+        (None, _, _) => {
             print_usage();
             std::process::exit(0);
         }
     };
 
-    let mut stdout = std::io::stdout().lock();
-    for i in start..end {
-        unsafe {
-            printf(FMT_PTR, i as i32);
+    let stdout = std::io::stdout().lock();
+    let mut writer = std::io::BufWriter::new(stdout);
+    const MAX_NUM_LEN: usize = 39;
+    let mut buf = [b'\n'; MAX_NUM_LEN];
+    let mut i = 0;
+    loop {
+        let mut curr = start + i * step;
+        if curr >= end {
+            break;
         }
+
+        let mut str_len = 0usize;
+        loop {
+            buf[MAX_NUM_LEN - str_len - 2] = usize_to_char_as_u8(curr % 10);
+            str_len += 1;
+            curr /= 10;
+            if curr == 0 {
+                break;
+            }
+        }
+        writer.write_all(&buf[MAX_NUM_LEN - str_len - 1..]).unwrap();
+        i += 1;
     }
-    stdout.flush().unwrap();
+    writer.flush().unwrap();
+}
+
+/// panics if the argument isnt a single digit
+fn usize_to_char_as_u8(num: usize) -> u8 {
+    match num {
+        0 => b'0',
+        1 => b'1',
+        2 => b'2',
+        3 => b'3',
+        4 => b'4',
+        5 => b'5',
+        6 => b'6',
+        7 => b'7',
+        8 => b'8',
+        9 => b'9',
+        _ => unreachable!(),
+    }
 }
 
 fn print_usage() {
@@ -32,4 +61,5 @@ fn print_usage() {
     println!("Usage:");
     println!("  range 5  --  prints a range from 0 (inclusive) to 5 (exclusive)");
     println!("  range 13 37  --  prints a range from 13 (inclusive) to 37 (exclusive)");
+    println!("  range 4 10 2 -- prints a range from 4 to 10, incrementing by 2");
 }
